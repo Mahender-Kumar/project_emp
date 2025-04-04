fcf2f8a23708eb39d79a34b0251ff71df9bdf1cc
Mahender Kumar <84126316+Mahender-Kumar@users.noreply.github.com>
2025-04-04 12:19:47 +0530

    refactor: enhance AppScaffold layout with adaptive navigation and improved body container styling

--- lib/presentation/views/app_scaffold.dart
+++ lib/presentation/views/app_scaffold.dart
@@ -29,12 +29,6 @@ class AppScaffold extends StatelessWidget {
       'label': "Search",
       'path': '/search',
     },
-    // {
-    //   'icon': const Icon(Icons.edit_note_rounded),
-    //   'selectedIcon': const Icon(Icons.edit_note_rounded),
-    //   'label': "Add",
-    //   'path': '/add',
-    // },
   ];
   @override
   Widget build(BuildContext context) {
@@ -44,17 +38,72 @@ class AppScaffold extends StatelessWidget {
     int selectedIndex = index == -1 ? 0 : index;
 
     return AdaptiveLayout(
+      primaryNavigation: SlotLayout(
+        config: <Breakpoint, SlotLayoutConfig>{
+          Breakpoints.mediumAndUp: SlotLayout.from(
+            key: const Key('Primary Navigation Medium'),
+            builder:
+                (_) => AdaptiveScaffold.standardNavigationRail(
+                  extended: false,
+                  width: 88,
+
+                  labelType: NavigationRailLabelType.all,
+                  leading: Padding(
+                    padding: const EdgeInsets.only(top: 16.0, bottom: 24.0),
+                    child: Icon(Icons.person_2),
+                  ),
+
+                  destinations: [
+                    ...navList.map(
+                      (e) => NavigationRailDestination(
+                        icon: e['icon'],
+                        selectedIcon: e['selectedIcon'],
+                        label: Text(
+                          e['label'],
+                          style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                      ),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (index) async {
+                    GoRouter.of(context).go(navList[index]['path'] ?? '/');
+                  },
+                  padding: const EdgeInsets.all(0),
+                ),
+          ),
+        },
+      ),
       body: SlotLayout(
         config: <Breakpoint, SlotLayoutConfig>{
           Breakpoints.smallAndUp: SlotLayout.from(
             key: const Key('Body All'),
-            builder: (_) => body,
+            builder: (_) {
+              final isSmallScreen = MediaQuery.of(context).size.width >= 600;
+              return Container(
+                decoration: BoxDecoration(
+                  border:
+                      isSmallScreen
+                          ? Border(
+                            left: BorderSide(
+                              width: 1.0,
+                              color:
+                                  Theme.of(context).colorScheme.outlineVariant,
+                            ),
+                          )
+                          : null,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: body,
+              );
+            },
           ),
         },
       ),
+
       bottomNavigation: SlotLayout(
         config: <Breakpoint, SlotLayoutConfig>{
-          Breakpoints.smallAndUp: SlotLayout.from(
+          Breakpoints.small: SlotLayout.from(
             key: const Key('Bottom Navigation Small'),
             inAnimation: AdaptiveScaffold.bottomToTop,
             outAnimation: AdaptiveScaffold.topToBottom,
