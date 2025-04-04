db471a3f0143efcd8674d6b081d8d1f4f7346509
Mahender Kumar <mksb1947@gmail.com>
2025-04-04 23:36:47 +0530

    refactor: simplify employee list layout by replacing ExpansionTile with SingleChildScrollView and improving structure

--- lib/presentation/views/home/components/employee_list.dart
+++ lib/presentation/views/home/components/employee_list.dart
@@ -11,7 +11,7 @@ class ExployeeListview extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverFillRemaining(
       child: Column(
-        // shrinkWrap: true,
+        mainAxisSize: MainAxisSize.min,
         children: [
           BlocBuilder<FetchEmployeeBloc, EmployeeState>(
             builder: (context, state) {
@@ -30,50 +30,106 @@ class ExployeeListview extends StatelessWidget {
                     state.employees.where((e) => e.isCurrent).toList();
                 final previousEmployees =
                     state.employees.where((e) => !e.isCurrent).toList();
-                return Column(
-                  children: [
-                    ExpansionTile(
-                      dense: true,
-                      initiallyExpanded: true,
-                      title: ListTile(
-                        dense: true,
-                        title: const Text('Current Employees'),
-                        // trailing: IconButton(icon: const Icon(Icons.history)),
-                      ),
-                      children: [
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: currentEmployees.length,
-                          itemBuilder: (context, index) {
-                            final employee = currentEmployees[index];
-                            return EmployeeTile(employee: employee);
-                          },
-                        ),
-                      ],
-                    ),
+                return SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        child: Column(
+                          children: [
+                            ListTile(
+                              tileColor:
+                                  Theme.of(context).colorScheme.surfaceBright,
+                              dense: true,
+                              title: const Text('Current Employees'),
+                            ),
 
-                    ExpansionTile(
-                      dense: true,
-                      initiallyExpanded: true,
-                      title: ListTile(
-                        dense: true,
-                        title: const Text('Previous Employees'),
-                        // trailing: IconButton(icon: const Icon(Icons.history)),
-                      ),
-                      children: [
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: previousEmployees.length,
-                          itemBuilder: (context, index) {
-                            final employee = previousEmployees[index];
-                            return EmployeeTile(employee: employee);
-                          },
+                            ListView.builder(
+                              padding: const EdgeInsets.all(0),
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: currentEmployees.length,
+                              itemBuilder: (context, index) {
+                                final employee = currentEmployees[index];
+                                return EmployeeTile(
+                                  employee: employee,
+                                  isDismissible: true,
+                                );
+                              },
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      // ExpansionTile(
+                      //   dense: true,
+                      //   initiallyExpanded: true,
+                      //   // tilePadding: EdgeInsets.all(0),
+                      //   // minTileHeight: 42,
+                      //   title: ListTile(
+                      //     dense: true,
+                      //     title: const Text('Current Employees'),
+                      //   ),
+                      //   children: [
+                      //     ListView.builder(
+                      //       shrinkWrap: true,
+                      //       physics: const NeverScrollableScrollPhysics(),
+                      //       itemCount: currentEmployees.length,
+                      //       itemBuilder: (context, index) {
+                      //         final employee = currentEmployees[index];
+                      //         return EmployeeTile(
+                      //           employee: employee,
+                      //           isDismissible: true,
+                      //         );
+                      //       },
+                      //     ),
+                      //   ],
+                      // ),
+                      Flexible(
+                        child: Column(
+                          children: [
+                            ListTile(
+                              dense: true,
+                              tileColor:
+                                  Theme.of(context).colorScheme.surfaceBright,
+                              title: const Text('Previous Employees'),
+                            ),
+                            ListView.builder(
+                              padding: const EdgeInsets.all(0),
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: previousEmployees.length,
+                              itemBuilder: (context, index) {
+                                final employee = previousEmployees[index];
+                                return EmployeeTile(employee: employee);
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      // ExpansionTile(
+                      //   dense: true,
+                      //   initiallyExpanded: true,
+                      //   title: ListTile(
+                      //     dense: true,
+                      //     title: const Text('Previous Employees'),
+                      //     // trailing: IconButton(icon: const Icon(Icons.history)),
+                      //   ),
+                      //   children: [
+                      //     ListView.builder(
+                      //       shrinkWrap: true,
+                      //       physics: const NeverScrollableScrollPhysics(),
+                      //       itemCount: previousEmployees.length,
+                      //       itemBuilder: (context, index) {
+                      //         final employee = previousEmployees[index];
+                      //         return EmployeeTile(employee: employee);
+                      //       },
+                      //     ),
+                      //   ],
+                      // ),
+                    ],
+                  ),
                 );
               } else {
                 return const Center(child: Text("No data available"));
--- lib/presentation/widgets/employee_tile.dart
+++ lib/presentation/widgets/employee_tile.dart
@@ -28,163 +28,154 @@ class EmployeeTile extends StatelessWidget {
   final FirestoreService firestoreService = FirestoreService();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Dismissible(
-          key: Key('${employee.id}'), // Unique key for dismissing
-          direction:
-              isDismissible == false
-                  ? DismissDirection.none
-                  : employee.isCurrent
-                  ? DismissDirection.horizontal
-                  : DismissDirection.endToStart, // Swipe from right to left
-          secondaryBackground: Container(
-            alignment: Alignment.centerRight,
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            color: Colors.red[600],
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(width: 20),
-                Text("Delete", style: TextStyle(fontWeight: FontWeight.w700)),
-                SizedBox(width: 8),
-                const Icon(Icons.delete_forever_outlined, color: Colors.white),
-              ],
+    return Dismissible(
+      key: Key('${employee.id}'), // Unique key for dismissing
+      direction:
+          isDismissible == false
+              ? DismissDirection.none
+              : employee.isCurrent
+              ? DismissDirection.horizontal
+              : DismissDirection.endToStart, // Swipe from right to left
+      secondaryBackground: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        color: Colors.red[600],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(width: 20),
+            Text("Delete", style: TextStyle(fontWeight: FontWeight.w700)),
+            SizedBox(width: 8),
+            const Icon(Icons.delete_forever_outlined, color: Colors.white),
+          ],
+        ),
+      ),
+      background: Container(
+        // alignment: Alignment.centerRight,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        color: Theme.of(context).colorScheme.onSecondary,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Icon(Icons.person_remove_alt_1_outlined, color: Colors.white),
+            SizedBox(width: 8),
+            Text(
+              "Terminate",
+              style: TextStyle(
+                // color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
             ),
-          ),
-          background: Container(
-            // alignment: Alignment.centerRight,
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            color: Theme.of(context).colorScheme.onSecondary,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Icon(Icons.person_remove_alt_1_outlined, color: Colors.white),
-                SizedBox(width: 8),
-                Text(
-                  "Terminate",
-                  style: TextStyle(
-                    // color: Colors.white,
-                    fontWeight: FontWeight.w700,
+            SizedBox(width: 20),
+          ],
+        ),
+      ),
+      confirmDismiss: (direction) async {
+        if (direction == DismissDirection.endToStart) {
+          return await showDialog(
+            context: context,
+            builder:
+                (context) => AlertDialog(
+                  title: const Text("Confirm Deletion"),
+                  content: const Text(
+                    "Are you sure you want to delete this employee?",
                   ),
-                ),
-                SizedBox(width: 20),
-              ],
-            ),
-          ),
-          confirmDismiss: (direction) async {
-            if (direction == DismissDirection.endToStart) {
-              return await showDialog(
-                context: context,
-                builder:
-                    (context) => AlertDialog(
-                      title: const Text("Confirm Deletion"),
-                      content: const Text(
-                        "Are you sure you want to delete this employee?",
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.of(context).pop(false),
-                          child: const Text("Cancel"),
-                        ),
-                        FilledButton(
-                          onPressed: () async {
-                            await firestoreService.deleteEmployee(employee);
-                            if (context.mounted) {
-                              Navigator.of(context).pop(true);
-                            }
-                          },
-                          child: const Text("Delete"),
-                        ),
-                      ],
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: const Text("Cancel"),
                     ),
-              );
-            }
-            if (direction == DismissDirection.startToEnd) {
-              return await showDialog(
-                context: context,
-                builder:
-                    (context) => AlertDialog(
-                      title: const Text("Confirm Termination"),
-                      content: const Text(
-                        "Are you sure you want to terminate this employee?",
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.of(context).pop(false),
-                          child: const Text("Cancel"),
-                        ),
-                        FilledButton(
-                          onPressed: () async {
-                            await firestoreService.terminateEmployee(employee);
-                            if (context.mounted) {
-                              Navigator.of(context).pop(true);
-                            }
-                          },
-                          child: const Text("Terminate"),
-                        ),
-                      ],
-                    ),
-              );
-            }
-            return null;
-          },
-          onDismissed: (direction) {},
-          child: ListTile(
-            dense: true,
-
-            title: Text(
-              employee.name,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(employee.position, style: const TextStyle(fontSize: 12)),
-                Text(
-                  !(employee.isCurrent)
-                      ? '${formatDate(employee.hireDate)} - ${formatDate(employee.leavingDate)}'
-                      : '${formatDate(employee.hireDate)}',
-                  style: const TextStyle(fontSize: 12),
-                ),
-              ],
-            ),
-            trailing:
-                showTrailingIcon
-                    ? PopupMenuButton(
-                      onSelected: (value) {
-                        switch (value) {
-                          case 'edit':
-                            context.pushNamed('edit', extra: employee);
-                            break;
-
-                          default:
+                    FilledButton(
+                      onPressed: () async {
+                        await firestoreService.deleteEmployee(employee);
+                        if (context.mounted) {
+                          Navigator.of(context).pop(true);
                         }
                       },
-                      itemBuilder: (context) {
-                        return [
-                          PopupMenuItem(
-                            value: 'edit',
-                            height: popupMenuButtonHeight,
-                            child: ListTile(
-                              dense: true,
-                              trailing: Icon(
-                                Icons.edit,
-                                size: listTileIconSize,
-                              ),
-                              title: Text('Edit'),
-                            ),
-                          ),
-                        ];
+                      child: const Text("Delete"),
+                    ),
+                  ],
+                ),
+          );
+        }
+        if (direction == DismissDirection.startToEnd) {
+          return await showDialog(
+            context: context,
+            builder:
+                (context) => AlertDialog(
+                  title: const Text("Confirm Termination"),
+                  content: const Text(
+                    "Are you sure you want to terminate this employee?",
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: const Text("Cancel"),
+                    ),
+                    FilledButton(
+                      onPressed: () async {
+                        await firestoreService.terminateEmployee(employee);
+                        if (context.mounted) {
+                          Navigator.of(context).pop(true);
+                        }
                       },
-                    )
-                    : null,
-          ),
+                      child: const Text("Terminate"),
+                    ),
+                  ],
+                ),
+          );
+        }
+        return null;
+      },
+      onDismissed: (direction) {},
+      child: ListTile(
+        dense: true,
+        title: Text(
+          employee.name,
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-      ],
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(employee.position, style: const TextStyle(fontSize: 12)),
+            Text(
+              !(employee.isCurrent)
+                  ? '${formatDate(employee.hireDate)} - ${formatDate(employee.leavingDate)}'
+                  : '${formatDate(employee.hireDate)}',
+              style: const TextStyle(fontSize: 12),
+            ),
+          ],
+        ),
+        trailing:
+            showTrailingIcon
+                ? PopupMenuButton(
+                  onSelected: (value) {
+                    switch (value) {
+                      case 'edit':
+                        context.pushNamed('edit', extra: employee);
+                        break;
+
+                      default:
+                    }
+                  },
+                  itemBuilder: (context) {
+                    return [
+                      PopupMenuItem(
+                        value: 'edit',
+                        height: popupMenuButtonHeight,
+                        child: ListTile(
+                          dense: true,
+                          trailing: Icon(Icons.edit, size: listTileIconSize),
+                          title: Text('Edit'),
+                        ),
+                      ),
+                    ];
+                  },
+                )
+                : null,
+      ),
     );
   }
 }
